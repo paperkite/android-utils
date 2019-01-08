2d06f737fdfa71090e06577c023ad6e6c7fae40e
Dylan Lange <dylan@paperkite.co.nz>
2019-01-08 15:58:01 +1300

    Remove comments

--- app/src/main/java/nz/co/paperkite/androidutils/app/MainViewModel.kt
+++ app/src/main/java/nz/co/paperkite/androidutils/app/MainViewModel.kt
@@ -1,21 +1,16 @@
 package nz.co.paperkite.androidutils.app
 
 import android.arch.lifecycle.MutableLiveData
-import android.util.Log
 import nz.co.paperkite.androidutils.arch.BaseViewModel
 import nz.co.paperkite.androidutils.arch.map
 
 class MainViewModel(private val initialCount: Int): BaseViewModel<Int>(initialCount) {
 	
 	private val _liveCount = MutableLiveData<Int>().apply { value = initialCount }
-	val liveCount = _liveCount map {
-		Log.d("MainViewModel", "Count: $it")
-		"Count: $it"
-	}
+	val liveCount = _liveCount map { "Count: $it" }
 	
 	fun incrementButtonClicked() {
 		val newCount = (_liveCount.value ?: initialCount) + 1
-		Log.d("MainViewModel", "newCount: $newCount")
 		_liveCount.postValue(newCount)
 	}
 	
